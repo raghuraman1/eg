c6646fb2563536215dd6de80d123978298ba4a4e
raghuraman1 <raghuraman1@yahoo.com>
2017-09-30 22:31:20 +0530

    save

---
+++ src/main/java/eg/HelloController.java
@@ -0,0 +1,49 @@
+package eg;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/url/{host}/{port}")
+    public String url(@PathVariable("host") String host, @PathVariable("port")
+    int port/*, @PathVariable("path") String path*/) throws IOException {
+    	/*path=path==null?"":path;*/
+    	String urlStr="http://"+host+":"+port;//+"/"+path;
+    	StringWriter sw= new StringWriter();
+    	PrintWriter pw= new PrintWriter(sw);
+    	URL url = new URL(urlStr);
+    	pw.println("url="+url);
+    	try(InputStream is = url.openStream();
+    			InputStreamReader isr= new InputStreamReader(is);
+    			BufferedReader br=new BufferedReader(isr);
+    			)
+    	{
+    		for (String line=br.readLine(); line!=null; line=br.readLine()) 
+    		{
+				pw.println(line);
+				
+			}
+    	}
+    	pw.flush();
+        return sw.getBuffer().toString();
+    }
+}
